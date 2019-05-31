e71ecd2a1d71565a6023cb031e88b20cb39f0c93
Anwesh43 <anweshthecool0@gmail.com>
2019-06-01 01:39:30 +0530

    adding DSBNode class

--- diagshiftboxview/src/main/java/com/anwesh/uiprojects/diagshiftboxview/DiagShiftBoxView.kt
+++ diagshiftboxview/src/main/java/com/anwesh/uiprojects/diagshiftboxview/DiagShiftBoxView.kt
@@ -129,4 +129,48 @@ class DiagShiftBoxView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class DSBNode(var i : Int, val state : State = State()) {
+
+        private var next : DSBNode? = null
+        private var prev : DSBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = DSBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawDSBNode(i, state.scale, paint)
+            prev?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : DSBNode {
+            var curr : DSBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
+        }
+    }
 }
