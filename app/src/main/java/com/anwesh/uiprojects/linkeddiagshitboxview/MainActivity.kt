d6bace0af2803e38ed72270bf018068a96f452d7
Anwesh43 <anweshthecool0@gmail.com>
2019-06-01 02:56:39 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkeddiagshitboxview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkeddiagshitboxview/MainActivity.kt
@@ -2,11 +2,18 @@ package com.anwesh.uiprojects.linkeddiagshitboxview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
